7df475eae524b8567e0fa5e6b46cda029cd44ae1
SzumekPL <mateusz.szumilas@wp.pl>
2023-11-09 15:06:12 +0100

    Created Methods
    Small refactors where taking code from main to functions

--- main.cpp
+++ main.cpp
@@ -5,15 +5,12 @@
 #include <chrono>
 #include <format>
 
-int main()
+short chooseOption(std::string optionName)
 {
     while (true)
     {
-        std::cout << "This is your final destination?\n";
-        std::cout << std::filesystem::current_path() << std::endl;
-
+        std::cout << optionName << "\n";
         std::cout << "Type y/n \n";
-
         std::string input;
         std::cin >> input;
 
@@ -22,71 +19,64 @@ int main()
 
         if (input == "y")
         {
-            std::filesystem::directory_entry entry{".\\ff15"};
-            std::cout << entry << "\n";
-            if(entry.exists())
-            {
-                std::cout << "file exist\n";
-            }
-            else
-            {
-                while (true)
-                {
-                    std::cout << "ff doesnt exist, do you want to init? (y/n)\n";
-
-                    std::string input;
-                    std::cin >> input;
-
-                    //transform string to lower case
-                    std::transform(input.begin(), input.end(), input.begin(), ::tolower);
-
-                    if (input == "y")
-                    {
-                        std::filesystem::create_directories("ff15");
-                        break;
-                    }
-                    else if ( input == "n" )
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        std::cout << "Unknow command, try again\n";
-                        std::cin.clear();
-                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                    }
-                }
-            }
-
-            const std::filesystem::path sandbox{"sandbox"};
-            std::filesystem::create_directories(sandbox/"dir1"/"dir2");
-            std::ofstream{sandbox/"file1.txt"};
-            std::ofstream{sandbox/"file2.txt"};
-
-
-            std::cout << "\nrecursive_directory_iterator:\n";
-            for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{sandbox})
-            {
-                std::filesystem::file_time_type ftime = std::filesystem::last_write_time(dir_entry);
-                std::cout << std::format("File write time is {}\n", ftime);
-                std::cout << dir_entry << '\n';
-            }
-
-            // delete the sandbox dir and all contents within it, including subdirs
-            //std::filesystem::remove_all(sandbox);
-            break;
+            return 1;
         }
         else if (input == "n")
         {
-            std::cout << "Do smth other" << std::endl;
-            break;
+            return 0;
         }
         else
         {
-            std::cout << "Unknow command, try again\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Type Y / n\n";
         }
+    }
+
+    return -1;
+}
+
+void createDirectory()
+{
+    std::filesystem::create_directories("ff15");
+}
+
+int main()
+{
+    short result = chooseOption(std::string("This is your final destination?\n") + std::filesystem::current_path().string());
+    if(result == -1)
+    {
+        std::cout << "Unexpected error in line " << __LINE__ << "\n";
+    }
+    else if(result == 0)
+    {
+        //choose other location
+    }
+    else if(result == 1)
+    {
+        std::filesystem::directory_entry entry{".\\ff15"};
+        std::cout << entry << "\n";
+        if(entry.exists())
+        {
+            //fiel exist and we can load information here
+        }
+        else
+        {
+            result = chooseOption("ff doesnt exist, do you want to init? (y/n)\n");
+            if(result == -1)
+            {
+                std::cout << "Unexpected error in line " << __LINE__ << "\n";
+            }
+
+            else if(result == 0)
+            {
+                std::cout << "OK so why u run this program?\n";
+            }
+            else if(result == 1)
+            {
+                createDirectory();
+                std::cout << "File created\n";
+            }
+        }
+
     }
 
     return 0;
