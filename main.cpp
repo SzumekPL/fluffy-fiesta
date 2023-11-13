90f39dd7999220d2227d47779743ab04087d2550
SzumekPL <mateusz.szumilas@wp.pl>
2023-11-13 18:50:25 +0100

    Add text file with all mod date
    Create a new folder with copy of init folder, and creating file with time of last modify of every regular file

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <chrono>
 
+
 short chooseOption(std::string optionName)
 {
     while (true)
@@ -33,25 +34,70 @@ short chooseOption(std::string optionName)
     return -1;
 }
 
-void createDirectory()
+inline bool createDirectory(std::filesystem::path fileName)
 {
-    std::filesystem::create_directories("ff15");
+    return std::filesystem::create_directories(fileName);
 }
+
+void initFFDirectory(std::filesystem::path dirPath)
+{
+    std::filesystem::copy(dirPath, dirPath / "ff15");
+
+    createDirectory(dirPath / "ff15/meta");
+    createDirectory(dirPath / "ff15/objects");
+
+    std::ofstream lastMod(dirPath / "ff15/meta/LastMod.txt");
+
+    for (auto entry = std::filesystem::recursive_directory_iterator(dirPath); entry != std::filesystem::recursive_directory_iterator(); ++entry)
+    {
+        if(std::filesystem::is_directory(entry->path()))
+        {
+            if(entry->path().filename().string()[0] == '.' || entry->path().filename().string() == "ff15")
+            {
+                entry.disable_recursion_pending();
+            }
+
+            continue;
+        }
+
+        lastMod << "File: " << entry->path().relative_path().string() << "\n";
+        lastMod << "Last Modified: " << std::filesystem::last_write_time(entry->path()) << "\n";
+        lastMod << "-----------------------------" << "\n";
+    }
+}
+
 
 int main()
 {
-    short result = chooseOption(std::string("This is your final destination?\n") + std::filesystem::current_path().string());
+    std::filesystem::path currentPath = std::filesystem::current_path();
+    short result = chooseOption(std::string("This is your final destination?\n") + currentPath.string());
     if(result == -1)
     {
         std::cout << "Unexpected error in line " << __LINE__ << "\n";
     }
-    else if(result == 0)
+    else if(result == 1 || result == 0)
     {
-        //choose other location
-    }
-    else if(result == 1)
-    {
-        std::filesystem::directory_entry entry{".\\ff15"};
+        if(result == 0)
+        {
+            do
+            {
+                std::cout << "Insert current path\n";
+                std::string input;
+                std::cin >> input;
+                if(!std::filesystem::exists(input))
+                {
+                    std::cout << "Path is incorrect, nothing here\n";
+                }
+                else
+                {
+                    currentPath = input;
+                    break;
+                }
+            }while(true);
+        }
+
+
+        std::filesystem::directory_entry entry{currentPath / "ff15"};
         std::cout << entry << "\n";
         if(entry.exists())
         {
@@ -71,8 +117,15 @@ int main()
             }
             else if(result == 1)
             {
-                createDirectory();
-                std::cout << "File created\n";
+                if(createDirectory(currentPath / "ff15"))
+                {
+                    std::cout << "File created\n";
+                    initFFDirectory(currentPath);
+                }
+                else
+                {
+                    std::cout << "I couldnt create a directory\n";
+                }
             }
         }
 
